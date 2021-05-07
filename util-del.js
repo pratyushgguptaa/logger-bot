72ff65c5e40f1f864232dfcf9eafc18f497f209a
pratyushgguptaa <56015449+pratyushgguptaa@users.noreply.github.com>
2021-05-08 02:40:46 +0530

    Give it a read

--- util-del.js
+++ util-del.js
@@ -1,14 +1,28 @@
 const Database = require('@replit/database')
 const db = new Database()
 
+/**
+ * To delete the records of any specified day from the logs
+ * of the author of the message
+ * if the number in the message cannot be parsed to int
+ * it replies accordingly
+ * @param {Message} msg Discord Message with prefix '++del'
+ */
 const del = async (msg) => {
-  day = parseInt(msg.content.split('++del')[1].trim())
-  userId = msg.author.id
-  userName = msg.author.tag
-  db.get(userId + '').then((logs) => {
+  day = parseInt(msg.content.split('++del')[1].trim()) || 0
+  if (day < 1) {
+    //if day entered is less than 1 or NaN
+    msg.reply(`Please enter a valid day number`)
+    return
+  }
+  //get the record of the author of the message
+  db.get(msg.author.id + '').then((logs) => {
+    //check if the logs are defined and not empty
     if (typeof logs.info !== 'undefined' && logs.info.length > 0) {
+      //filter out the day to be removed
       logs.info = logs.info.filter((log) => log.date != day)
 
+      //update the record with the filtered logs
       db.set(msg.author.id + '', logs)
 
       msg.reply(`You have successfully deleted all the logs for day ${day}`)
@@ -16,22 +30,34 @@ const del = async (msg) => {
       msg.reply(`You have no active logs. Pls start logging by \`++add {log}\``)
   })
 }
-
-const delAll = async (msg) => {
-  db.get(msg.author.id + '')
-    .then(() => {
-      db.delete(msg.author.id + '').then(() =>
-        msg.reply(`All your data has been deleted.`)
-      )
-    })
+/**
+ * To delete the record from the database with the key as the id of the
+ * author of the message passed and reply accordingly
+ * @param {Message} msg Discord message which has been approved for
+ * deletion of the record associated with the author of the message
+ */
+const delAll = async (msg) =>
+  db
+    .get(msg.author.id + '')
+    .then(() =>
+      db
+        .delete(msg.author.id + '')
+        .then(() => msg.reply(`All your data has been deleted.`))
+    )
     .catch(() =>
       msg.reply(
         'There was some issue during resetting your data. Please try again.'
       )
     )
-}
 
+/**
+ * To confirm with the message author to resest his/her information and
+ * all the logs present in the database
+ * It waits for 10 seconds for a reply from the author and acts accordingly
+ * @param {Message} msg Discord Message with prefix '++reset'
+ */
 const reset = async (msg) => {
+  //filter to avoid reading messages from different users/bots
   let filter = (m) => m.author.id === msg.author.id
   msg.channel
     .send(
@@ -39,17 +65,21 @@ const reset = async (msg) => {
     )
     .then(() => {
       msg.channel
+        //Similar to createMessageCollector but in promise form.
+        //Resolves with a collection of messages that pass the specified filter.
         .awaitMessages(filter, {
-          max: 1,
-          time: 10000,
-          errors: ['time'],
+          max: 1, //only one message to be read
+          time: 10000, // and for 10 seconds only
+          errors: ['time'], //stop/end reason to cause the promise to reject
         })
         .then((message) => {
+          //when getting a response
           message = message.first()
           if (
             message.content.toUpperCase() == 'YES' ||
             message.content.toUpperCase() == 'Y'
           )
+            //Deleting all the records for author
             delAll(msg)
           else if (
             message.content.toUpperCase() == 'NO' ||
@@ -58,6 +88,7 @@ const reset = async (msg) => {
             message.channel.send(`Delete Process Terminated`)
           else message.channel.send(`Terminated: Due to Invalid Response`)
         })
+        //catch the error thrown on timeout
         .catch(() => {
           msg.channel.send('Buddy Timeout! Please try again.')
         })
--- util-functions.js
+++ util-functions.js
@@ -1,19 +1,45 @@
+/**
+ * To calculate the current streak
+ * We start from the end i.e., the current that should be logged
+ * to maintain the streak and check for consecutive days
+ * if previous consecutive day is not present break out of the loop
+ * and return the streak
+ * @param {info[]} logs record.info array with all the records
+ * @param {number} startDate record.startDate time in milliseconds of the first record
+ * @returns {Promise<number>} streak in number of days
+ */
 const calculateStreak = async (logs, startDate) => {
+  // if empty array
   if (logs.length < 1) return 0
+  //to get the day count for current time that should be logged
   day = await totalDays(startDate)
   str = 1
   n = logs.length - 1
+  
+  //if log for current day is not present streak = 0
   if (day != logs[n].date) return 0
   else {
+    //loop backwards and check for consecutive days
     while (n--)
+      //if consecutive day found update @variable day and str
       if (day - logs[n].date == 1) {
         day = logs[n].date
         str++
-      } else if (day - logs[n].date != 0) break
+      }
+      //if not consecutive day and not even the same day then streak break
+      else if (day - logs[n].date != 0) break
     return str
   }
 }
 
+/**
+ * To calculate the maximum streak that was achieved in the given
+ * array of logs, same way as @function calculateStreak but not breaking the loop,
+ * instead we just check if the streak is greater than previous maximum streak.
+ * @param {info[]} logs record.info array of all logs
+ * @param {number} startDate record.startDate time in milliseconds of the first log
+ * @returns {Promise<number>} the maximum streak achieved in the logs
+ */
 const maxStreak = async (logs, startDate) => {
   if (logs.length < 1) return 0
   curr = 1
@@ -21,19 +47,37 @@ const maxStreak = async (logs, startDate) => {
   max = 0
   day = logs[n].date
   while (n--) {
+    //if consecutive day streak plus 1
     if (day - logs[n].date == 1) curr++
+    //if not same day check if streak greater than max streak
     else if (day - logs[n].date != 0) {
       if (curr > max) max = curr
       curr = 1
     }
+    //update day for each record
     day = logs[n].date
   }
   return max > curr ? max : curr
 }
 
+/**
+ * To calculate the number of days it has been passed
+ * since the day of first log, we subtract to get the number of milliseconds
+ * then divide by the number of milliseconds in a day (24*60*60*1000),
+ * adding 1 to get the number of days and return its floor value
+ * @param {number} startDate record.startDate time in milliseconds since first log
+ * @returns {Promise<number>} current day since first log
+ */
 const totalDays = async (startDate) =>
   Math.floor(1 + (Date.now() - startDate) / (1000 * 3600 * 24))
 
+/**
+ * to calculate all distinct days that has been logged for the array
+ * we move left from right and count for all the days that were not
+ * equal to the current day
+ * @param {info[]} logs record.info array of all logs
+ * @returns {Promise<number>} number of days logged
+ */
 const totalDistinct = async (logs) => {
   if (logs.length < 1) return 0
   n = logs.length - 1
